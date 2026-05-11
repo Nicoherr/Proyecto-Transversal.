aca47dc04900cfa39a43f14d002b06bd72acdb80
Nicolas Herrera Labra <nico.herreral@duocuc.cl>
2026-05-11 12:02:14 -0400

    clase lunes 11

--- notificacion/src/main/java/com/marketplace/notificacion/security/SecurityConfig.java
+++ notificacion/src/main/java/com/marketplace/notificacion/security/SecurityConfig.java
@@ -1,17 +1,14 @@
+package com.marketplace.notificacion.security;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,8 +19,8 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/notificacion/**").authenticated()
-                        .requestMatchers("/notificacion/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/reporte/**").authenticated()
+                        .requestMatchers("/reporte/**").hasRole("USER")
                         .anyRequest().authenticated()
                 );
         return http.build();
