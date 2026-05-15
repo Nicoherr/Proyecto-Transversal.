dcc76416e13838fb90cbb1972fcf5ec2e98c2752
danaenicol <dan.aliante@duocuc.cl>
2026-05-15 13:23:35 -0400

    agrego migration

---
+++ inventario/src/main/resources/db/migration/V1__createtables.sql
@@ -0,0 +1,10 @@
+create table inventario (
+   active bit not null,
+   id bigint not null auto_increment,
+   email varchar(100) not null,
+   password varchar(255) not null,
+   primary key (id)
+) engine=InnoDB;
+
+alter table inventario
+    add constraint UK6dotkott2kjsp8vw4d0m25fb7 unique (email);
---
+++ inventario/src/main/resources/db/migration/V2__load_default_data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `marketplace_inventario`.`inventario` (`active`, `email`, `password`, `role`) VALUES (b'1', 'rob.morenop@profesor.duoc.cl', '$2b$12$okPQajXIw4Qv4l/ZADKMg.p5oLNapMJZv946hp9qm6o3Nfgxs8uXO', 'ADMIN');
